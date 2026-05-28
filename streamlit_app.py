6c9037473f7b260793e3ff5a5166086b7b2632df
nailasyairi-lgtm <nailasyairi@gmail.com>
2026-05-28 07:42:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,33 @@
-import streamlit as st
+st.title("🧪 VirtualChem Lab")
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.write("Simulasi Praktikum Kimia Virtual")
+
+menu = st.selectbox(
+    "Pilih Praktikum",
+    ["Menimbang", "Titrasi"]
 )
+
+# Simulasi Menimbang
+if menu == "Menimbang":
+
+    st.header("Simulasi Menimbang")
+
+    massa = st.number_input(
+        "Masukkan massa (gram)",
+        min_value=0.0
+    )
+
+    if st.button("Timbang"):
+        st.success(
+            f"Berhasil menimbang {massa} gram"
+        )
+
+# Simulasi Titrasi
+if menu == "Titrasi":
+
+    st.header("Simulasi Titrasi")
+
+    if st.button("Mulai Titrasi"):
+        st.info(
+            "Larutan berubah warna menjadi pink"
+        )
