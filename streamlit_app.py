f7f99b6507c71a7b308265d182f2c279db7da848
nailasyairi-lgtm <nailasyairi@gmail.com>
2026-05-28 10:46:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,9 +59,9 @@ st.title("Simulasi Menimbang Kimia")
 # Input target dan massa saat ini
 target_massa = 0.03
 massa_timbangan = st.number_input("Masukkan massa (gram):", min_value=0.0, step=0.01, format="%.2f")
-for 0 in range (5):
-if st.button("Timbang", key=f"timbang_{0}"):
-    st.write(f"Tombol ke-{0} ditekan")
+for n in range (5):
+if st.button("Timbang", key="tombol_timbang_utama"):
+    st.write("Tombol ditekan")
     # Logika pengecekan
     if massa_timbangan == target_massa:
         st.success(f"Penimbangan Selesai! Berhasil menimbang {massa_timbangan} gram.")
