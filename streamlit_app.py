d3e0dcf3e20aa1b502d11738eacab64d9ba13c9a
nailasyairi-lgtm <nailasyairi@gmail.com>
2026-05-28 09:23:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,24 @@ if menu == "Titrasi":
         st.info(
             "Larutan berubah warna menjadi pink"
         )
+<!DOCTYPE html>
+<html>
+<head>
+    <title>VirtualChem Lab</title>
+    <link rel="stylesheet" href="{{ url_for('static', filename='style.css') }}">
+</head>
+
+<body>
+
+<h1>VirtualChem Lab</h1>
+
+<div class="menu">
+    <a href="/timbang">Simulasi Menimbang</a>
+</div>
+
+<div class="menu">
+    <a href="/titrasi">Simulasi Titrasi</a>
+</div>
+
+</body>
+</html>
